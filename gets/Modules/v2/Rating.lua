1e41c72db50118fb32a3255ba1acfeca4206ab7f
Arthur Peres <arthurpluzperes@gmail.com>
2025-12-10 17:36:45 -0400

    Add initial implementation of Rating module as a placeholder

---
+++ gets/Modules/v2/Rating.lua
@@ -0,0 +1,6 @@
+-- Rating.lua
+-- Este é um placeholder. Substitua pelo código original do Rating.lua se necessário.
+
+return function()
+    print("Rating module carregado. Substitua este arquivo pelo código original para funcionalidade completa.")
+end
